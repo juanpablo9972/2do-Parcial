51a446d40e0db4893291969eb109352a3cf7ed7c
visual_community@outlook.com <visual_community@outlook.com>
2019-11-30 12:33:16 -0400

    Fin Segundo Parcial

---
+++ Soruco/Prueba_Parcial/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prueba_Parcial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prueba_Parcial")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce257c15-1747-4a96-af84-450ab4ae38e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Soruco/Prueba_Parcial/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Soruco;
+namespace Prueba_Parcial
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Save()
+        {
+            //organizar
+            Company person = new Company("Juan", 1000);
+            double valoresperado = 1500;
+            //actuar
+            person.Save(500);
+            double valorobtenido = person.SaveProject;
+            //afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+
+        [TestMethod]
+        public void Spend()
+        {
+            //organizar
+            Company person = new Company("Juan", 1000);
+            double valoresperado = 500;
+            //actuar
+            person.Spend(500);
+            double valorobtenido = person.SaveProject;
+            //afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+        [TestMethod]
+        public void ChangeName()
+        {
+            //organizar
+            Company person = new Company("Juan", 1000);
+            string valoresperado = "Pedro" ;
+            //actuar
+            person.ChangeName("Pedro");
+            string valorobtenido = person.Name;
+            //afirmar
+            Assert.AreEqual(valoresperado, valorobtenido);
+        }
+    }
+}
